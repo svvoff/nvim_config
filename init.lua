924c7e32b378c8026449620280418fa84075a1ff
Сорокин Андрей Сергеевич <anssorokin@domclick.ru>
2025-10-15 20:52:41 +0200

    update lines number displaying

--- init.lua
+++ init.lua
@@ -25,5 +25,5 @@ vim.api.nvim_set_keymap(  't'  ,  '<Leader><ESC>'  ,  '<C-\\><C-n>'  ,  {noremap
 
 vim.wo.number = true
 vim.wo.relativenumber = true
-vim.opt.statuscolumn = "%s %{v:lnum} %{v:relnum}"
+vim.opt.statuscolumn = "%s %{v:lnum} %{v:relnum} "
 
